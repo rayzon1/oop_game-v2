c0fc1a2d90b997457f92c4967d372cb33c9448cf
Gerardo Keys <jotojry@icloud.com>
2019-04-25 16:38:43 -0700

    second commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  * app.js */
 
  const game = new Game();
- const phrase = new Phrase(game.phrases);
+ const phrase = new Phrase();
 
  game.phrases.forEach((phrase, index) => {
      console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
